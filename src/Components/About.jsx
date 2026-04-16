06488a51d067e8a034ce4dfda5e27afec8a5020b
sabba1010 <sabbahossain@gmail.com>
2026-04-16 20:25:11 +0600

    adddd

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import profileImg from "../assets/sabba.png";
+import profileImg from "../assets/Sabba.png";
 
 const About = () => {
   return (
